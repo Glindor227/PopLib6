be4f086ba04c924e06cdfabc66cd80bf32b610fe
Андрей <glindor227@gmail.com>
2020-05-12 20:17:01 +0300

    классы Red, White, Green. В классе Green есть метод show, который возвращает строчку “green”. Классы Red и White в конструкторе выводят в log эту строчку. Решить без применения di и dagger. Вывести в log объекты этих классов

---
+++ app/src/main/java/com/geekbrains/poplib6/White.java
@@ -0,0 +1,11 @@
+package com.geekbrains.poplib6;
+
+import android.util.Log;
+
+class White {
+    White() {
+        Green green = new Green();
+        Log.d("White","Green message="+green.show());
+        Log.d("White","Green class="+green.toString());
+    }
+}
